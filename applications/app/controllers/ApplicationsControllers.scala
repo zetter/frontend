6d864d9c5294b559e7c30324aace6435a99ce3d7
GHaberis <george.haberis@theguardian.com>
2017-10-10 15:06:47 +0100

    create new route for font loader

--- applications/app/controllers/ApplicationsControllers.scala
+++ applications/app/controllers/ApplicationsControllers.scala
@@ -39,6 +39,7 @@ trait ApplicationsControllers {
   lazy val indexController = wire[IndexController]
   lazy val siteVerificationController = wire[SiteVerificationController]
   lazy val shareCountController = wire[ShareCountController]
+  lazy val fontLoaderController = wire[FontLoaderController]
 
   //A fake geolocation controller to test it locally
   lazy val geolocationController = wire[FakeGeolocationController]
