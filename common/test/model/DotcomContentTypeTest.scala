a961d8d8bfb7b06e096032225f0fecc10317815c
Thomas Bonnin <thomas.bonnin@gmail.com>
2017-10-27 14:08:26 +0100

    Add tests for DotcomContentTypes

---
+++ common/test/model/DotcomContentTypeTest.scala
@@ -0,0 +1,33 @@
+package model
+
+import org.scalatest.{FlatSpec, Matchers}
+import play.api.libs.json.Json
+
+class DotcomContentTypeTest extends FlatSpec with Matchers {
+
+  "A Content type" should "have a name" in {
+    DotcomContentType.Article.name should be("Article")
+    DotcomContentType.NetworkFront.name should be("Network Front")
+    DotcomContentType.Identity.name should be("userid")
+  }
+
+  "Content types" should "be encodable in json" in {
+    val types = Seq(
+      DotcomContentType.Survey,
+      DotcomContentType.NetworkFront,
+      DotcomContentType.Identity
+    )
+    val expectedJson = Json.parse("""["Survey", "Network Front", "userid"]""")
+    Json.toJson(types) should be(expectedJson)
+  }
+
+  "Content types" should "be decoded from json" in {
+    val expectedTypes = Seq(
+      DotcomContentType.TagIndex,
+      DotcomContentType.NetworkFront,
+      DotcomContentType.Identity
+    )
+    Json.parse("""["Index", "Network Front", "userid"]""").as[Seq[DotcomContentType]] should be(expectedTypes)
+  }
+
+}
