eddaaa131c55e5c4fc0e4784b6d6bb1e77b03f5c
James Byers <Ap0c@users.noreply.github.com>
2018-04-23 15:23:22 +0100

    Properly manage state.

--- static/src/javascripts/projects/common/modules/ui/accessibility-menu.js
+++ static/src/javascripts/projects/common/modules/ui/accessibility-menu.js
@@ -23,7 +23,9 @@ type MenuPropTypes = {
 // ----- Components ----- //
 
 const MenuToggle = (props: MenuTogglePropTypes) => {
-    const onClick = () => props.toggle(!props.isOpen);
+    const onClick = () => {
+        props.toggle(!props.isOpen);
+    };
 
     return (
         <a
@@ -34,71 +36,87 @@ const MenuToggle = (props: MenuTogglePropTypes) => {
     );
 };
 
-const DropDown = (props: MenuPropTypes) => (
-    <ul className="dropdown-menu dropdown-menu--open">
-        <li className="dropdown-menu__item">
-            <label className="dropdown-menu__title" htmlFor="a11y-menu-night">
-                <input id="a11y-menu-night" type="checkbox" />
-                Night Mode
-            </label>
-        </li>
-        <li className="dropdown-menu__item">
-            <label className="dropdown-menu__title" htmlFor="a11y-menu-font">
-                <input id="a11y-menu-font" type="checkbox" />
-                Readable Font
-            </label>
-        </li>
-        <li className="dropdown-menu__item">
-            <label
-                className="dropdown-menu__title"
-                htmlFor="a11y-menu-background">
-                <input id="a11y-menu-background" type="checkbox" />
-                Yellow Background
-            </label>
-        </li>
-        <li className="dropdown-menu__item">
-            <label
-                className="dropdown-menu__title"
-                htmlFor="a11y-menu-lineheight">
-                <input id="a11y-menu-lineheight" type="checkbox" />
-                Line Height
-            </label>
-        </li>
-        <li className="dropdown-menu__item">
-            <label
-                className="dropdown-menu__title"
-                htmlFor="a11y-menu-animation">
-                <input id="a11y-menu-animation" type="checkbox" />
-                Reduce Animation
-            </label>
-        </li>
-    </ul>
-);
+const DropDown = (props: MenuPropTypes) => {
+    if (!props.isOpen) {
+        return <span />;
+    }
 
-const AccessibilityMenu = (props: MenuTogglePropTypes) => (
-    <div>
-        <MenuToggle isOpen={props.isOpen} toggle={props.toggle} />
-        <DropDown isOpen={props.isOpen} />
-    </div>
-);
+    return (
+        <ul className="dropdown-menu dropdown-menu--open">
+            <li className="dropdown-menu__item">
+                <label
+                    className="dropdown-menu__title"
+                    htmlFor="a11y-menu-night">
+                    <input id="a11y-menu-night" type="checkbox" />
+                    Night Mode
+                </label>
+            </li>
+            <li className="dropdown-menu__item">
+                <label
+                    className="dropdown-menu__title"
+                    htmlFor="a11y-menu-font">
+                    <input id="a11y-menu-font" type="checkbox" />
+                    Readable Font
+                </label>
+            </li>
+            <li className="dropdown-menu__item">
+                <label
+                    className="dropdown-menu__title"
+                    htmlFor="a11y-menu-background">
+                    <input id="a11y-menu-background" type="checkbox" />
+                    Yellow Background
+                </label>
+            </li>
+            <li className="dropdown-menu__item">
+                <label
+                    className="dropdown-menu__title"
+                    htmlFor="a11y-menu-lineheight">
+                    <input id="a11y-menu-lineheight" type="checkbox" />
+                    Line Height
+                </label>
+            </li>
+            <li className="dropdown-menu__item">
+                <label
+                    className="dropdown-menu__title"
+                    htmlFor="a11y-menu-animation">
+                    <input id="a11y-menu-animation" type="checkbox" />
+                    Reduce Animation
+                </label>
+            </li>
+        </ul>
+    );
+};
+
+class AccessibilityMenu extends React.Component<{}, State> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        };
+    }
+
+    toggle(isOpen: boolean) {
+        this.setState({ isOpen });
+    }
+
+    render() {
+        return (
+            <div>
+                <MenuToggle
+                    isOpen={this.state.isOpen}
+                    toggle={this.toggle.bind(this)}
+                />
+                <DropDown isOpen={this.state.isOpen} />
+            </div>
+        );
+    }
+}
 
 const initAccessibilityMenu = () => {
     const a11yMenu = document.getElementById('a11y-menu');
 
     if (a11yMenu) {
-        const state: State = {
-            isOpen: false,
-        };
-
-        render(
-            <AccessibilityMenu
-                isOpen={state.isOpen}
-                toggle={(isOpen: boolean) => {
-                    state.isOpen = isOpen;
-                }}
-            />,
-            a11yMenu
-        );
+        render(<AccessibilityMenu />, a11yMenu);
     }
 };
 
