6b513ae3918e8a23858d4f91f7ce1e0d7647d72f
Chris Zetter <zetter@users.noreply.github.com>
2018-11-14 16:59:43 +0000

    Remove uneeded lib files

--- static/src/javascripts/lib/detect.js
+++ static/src/javascripts/lib/detect.js
@@ -287,16 +287,6 @@ const pageVisible = (): boolean => pageVisibility === 'visible';
 
 const isEnhanced = (): boolean => window.guardian.isEnhanced;
 
-const adblockInUse: Promise<?boolean> = new Promise(resolve => {
-    if (window.guardian.adBlockers.hasOwnProperty('active')) {
-        // adblock detection has completed
-        resolve(window.guardian.adBlockers.active);
-    } else {
-        // Push a listener for when the JS loads
-        window.guardian.adBlockers.onDetect.push(resolve);
-    }
-});
-
 const getReferrer = (): string => document.referrer || '';
 
 const getUserAgent = ((): string | { browser: string, version: string } => {
@@ -359,6 +349,5 @@ export {
     pageVisible,
     breakpoints,
     isEnhanced,
-    adblockInUse,
     getReferrer,
 };
