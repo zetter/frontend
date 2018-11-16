3b010433e067e2253667385d70a6c26d755756b0
Chris Zetter <zetter@users.noreply.github.com>
2018-11-16 17:09:07 +0000

    Remove unneeded development dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -78,20 +77,6 @@ module.exports = {
         ],
     },
     plugins: [
-        // Makes videosjs available to all modules in the videojs chunk.
-        // videojs plugins expect this object to be available globally,
-        // but it's sufficient to scope it at the chunk level
-        new webpack.ProvidePlugin({
-            videojs: 'videojs',
-        }),
-
-        new CircularDependencyPlugin({
-            // exclude detection of files based on a RegExp
-            exclude: /node_modules/,
-            // add errors to webpack instead of warnings
-            failOnError: true,
-        }),
-
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
